2a92e679c81afa4ad77dd77f871848b276f9bc24
IonStreak <christopherl120@eagles.hba.net>
2017-06-19 14:58:03 -1000

    Not finished

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 function fortuneTeller(){
- hu  
+	console.log("Greetings.");
+	var numOfChildren = -10;
+	var partnerName= "The Worst Person Ever. E V E R . ";
+	var geographicalLocation= "an NYC Slum";
+	var jobTitle= "rat farmer"
+
+	console.log ("You will be a X in Y, and married to Z with N kids.");
+	console.log ("You will be a " + jobTitle + " in " + geographicalLocation + " , and married to " + partnerName + " with " + numOfChildren + "kids.");
 }
 
 function ageCalculator(){
